37a88bbd4d98f51049ecd35e54f92dbaae49a1a8
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-08-02 11:55:07 -0300

    Initial commit.

--- src/main/java/com/trybe/idadeemdias/Main.java
+++ src/main/java/com/trybe/idadeemdias/Main.java
@@ -2,12 +2,39 @@ package com.trybe.idadeemdias;
 
 import java.util.Scanner;
 
+/** Classe Principal. */
 public class Main {
 
-	public static void main(String[] args) {
-		
+  /** Classe Principal. */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-		
-	}
-
+    System.out.println("Qual é o seu nome?");
+    final String nome = scan.next();
+    
+    System.out.println("Qual é sua idade em anos, meses e dias?");
+    System.lineSeparator();
+    System.out.println("anos:");
+    final int ano = scan.nextInt();
+    
+    System.out.println("meses:");
+    final int mes = scan.nextInt();
+    
+    System.out.println("dias:");
+    final int dia = scan.nextInt();
+    
+    scan.close();
+    
+    Pessoa pessoa = new Pessoa();
+    String idade = Integer.toString(pessoa.calcularIdadeEmDias(ano, mes, dia));
+    
+    StringBuilder stringBuilder = new StringBuilder();
+    StringBuilder resultado = stringBuilder.append("Olá ")
+                                           .append(nome)
+                                           .append(", sua idade em dias é ")
+                                           .append(idade);
+    System.out.print(resultado);
+  }
 }
+
+
--- src/main/java/com/trybe/idadeemdias/Pessoa.java
+++ src/main/java/com/trybe/idadeemdias/Pessoa.java
@@ -1,7 +1,20 @@
 package com.trybe.idadeemdias;
 
+/**
+ * Classe Pessoa.
+ */
 public class Pessoa {
-	
 
-	
+  /**
+   * retorna idade em dias ex: 1231312.
+   */
+  public int calcularIdadeEmDias(int ano, int mes, int dia) {
+    int idadeAno = ano * 365;
+    int idadeMes = mes * 30;
+    int idadeDia = dia;
+    int resultado = idadeAno + idadeMes + idadeDia;
+
+    return resultado;
+  }
+
 }
